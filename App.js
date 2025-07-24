d9404c20bb836ff0f4b675d9154088fe8330030f
Foster Amponsah <foster.asante@gwosevo.com>
2025-07-24 09:52:21 +0000

    Update

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './src/screens/HomeScreen';
+import HomeScreen from './src/screens/dashboard/HomeScreen';
 import DetailsScreen from './src/screens/DetailsScreen';
 import LoginScreen from './src/screens/auth/LoginScreen';
 import OnboardingScreen from './src/screens/OnboardingScreen';
